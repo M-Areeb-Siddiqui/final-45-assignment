c333dd258bf38f0d0d370fa3b6ff7cbf509cec45
M-Areeb-Siddiqui <m.areeb986@gmail.com>
2024-03-09 09:17:44 -0800

    first commit

---
+++ assignment 3/main.js
@@ -0,0 +1,5 @@
+//storing a famous quote and its author
+var quote = "You cannot convince anyone for their own benefit";
+var author = "Muhammad Areeb";
+//printing the message
+console.log("", quote, "-", "author");
---
+++ assignment 3/main.ts
@@ -0,0 +1,5 @@
+//storing a famous quote and its author
+let quote: string= "You cannot convince anyone for their own benefit"
+let author: string= "Muhammad Areeb"
+//printing the message
+console.log("",quote,"-","author");
